64fc0e0bf2d1f7f168184b49a1b70ccbf42459ff
undersquire <apexdev23@gmail.com>
2021-01-11 13:41:41 -0800

    merged all into gui.c

--- gui/README.txt
+++ gui/README.txt
@@ -1,4 +1,4 @@
 gui.h:
 	description: a simple, lightweight, easy to use, cross platform gui library
-	author: undersquire
+	authors: undersquire, MousieDev
 	version: 1.0.0
--- gui/gui.c
+++ gui/gui.c
@@ -1,4 +1,4 @@
-/* gui_win32.c - gui.h implementation for win32
+/* gui.c - gui.h platform implementation
  *
  * Copyright (c) 2021 Cleanware
  *
@@ -19,8 +19,20 @@
  * 3. This notice may not be removed or altered from any source distribution.
 */
 
+#if defined(__linux__)
+    #if !defined(WAYLAND) && !defined(X11)
+        #define X11
+    #endif
+#endif
+
 #include "gui.h"
-#include <stdlib.h>
+
+/*----------------------------------------------------------------------------*/
+/*                           Windows Implementation                           */
+/*----------------------------------------------------------------------------*/
+
+#if defined(_WIN32)
+
 #include <windows.h>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam)
@@ -248,3 +260,149 @@ int poll_event(struct window_event *event)
     }
     else return 0;
 }
+
+#endif
+
+/*----------------------------------------------------------------------------*/
+/*                             UWP Implementation                             */
+/*----------------------------------------------------------------------------*/
+
+#if defined(_WIN32) && defined(UWP)
+
+
+
+#endif
+
+/*----------------------------------------------------------------------------*/
+/*                             X11 Implementation                             */
+/*----------------------------------------------------------------------------*/
+
+#if defined(__linux__) && defined(X11)
+
+#include <stdlib.h>
+#include <X11/Xlib.h>
+
+struct window
+{
+    Window window;
+    Display *display;
+};
+
+window_t open_window(char *title)
+{
+    Display *display = XOpenDisplay(NULL);
+
+    if(display == NULL)
+        return NULL;
+
+    int screen = DefaultScreen(display);
+
+    Window window = XCreateSimpleWindow(
+        display,
+        RootWindow(display, screen),
+        0,
+        0,
+        640,
+        480,
+        1,
+        BlackPixel(display, screen),
+        WhitePixel(display, screen)
+    );
+
+    XStoreName(display, window, title);
+
+    XSelectInput(display, window, ExposureMask | KeyPressMask | KeyReleaseMask);
+    XMapWindow(display, window);
+
+    struct window *wnd = (struct window *)malloc(sizeof(struct window));
+
+    wnd->window = window;
+    wnd->display = display;
+
+    return (window_t)wnd;
+}
+
+void close_window(window_t window)
+{
+    struct window *wnd = (struct window *)window;
+
+    XDestroyWindow(wnd->display, wnd->window);
+    free(window);
+}
+
+void read_window(window_t window, struct window_attr *attr)
+{
+    struct window *wnd = (struct window *)window;
+
+    XWindowAttributes xattr;
+    XGetWindowAttributes(wnd->display, wnd->window, &xattr);
+
+    char *name = "\0";
+
+    XFetchName(wnd->display, wnd->window, &name);
+
+    int i;
+    for(i = 0; i < 64; i++)
+    {
+        if(name[i] == '\0')
+        {
+            attr->title[i] = '\0';
+            break;
+        }
+
+        attr->title[i] = name[i];
+    }
+
+    attr->x = xattr.x;
+    attr->y = xattr.y;
+    attr->width = xattr.width;
+    attr->height = xattr.height;
+}
+
+void write_window(window_t window, struct window_attr *attr)
+{
+    struct window *wnd = (struct window *)window;
+
+    XStoreName(wnd->display, wnd->window, attr->title);
+
+    XWindowChanges changes;
+
+    changes.x = attr->x;
+    changes.y = attr->y;
+    changes.width = attr->width;
+    changes.height = attr->height;
+
+    XConfigureWindow(wnd->display, wnd->window, CWX | CWY | CWWidth | CWHeight, &changes);
+}
+
+#endif
+
+/*----------------------------------------------------------------------------*/
+/*                           Wayland Implementation                           */
+/*----------------------------------------------------------------------------*/
+
+#if defined(__linux__) && defined(WAYLAND)
+
+
+
+#endif
+
+/*----------------------------------------------------------------------------*/
+/*                            Cocoa Implementation                            */
+/*----------------------------------------------------------------------------*/
+
+#if defined(__APPLE__)
+
+
+
+#endif
+
+/*----------------------------------------------------------------------------*/
+/*                           Android Implementation                           */
+/*----------------------------------------------------------------------------*/
+
+#if defined(__ANDROID__)
+
+
+
+#endif
